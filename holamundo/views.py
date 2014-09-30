6bee0472e2e7afc5f828911e175196f59c1d4119
israel <asralf@gmail.com>
2014-09-30 15:01:43 -0500

    Camb io de plantilla de inicio para prueba de Google API

--- holamundo/views.py
+++ holamundo/views.py
@@ -1,7 +1,7 @@
 from pyramid.view import view_config
 
 
-@view_config(route_name='home', renderer='templates/WMSImage.pt')
+@view_config(route_name='home', renderer='templates/GoogleAPI.pt')
 def my_view(request):
     return {'project': 'HolaMundo'}
 
